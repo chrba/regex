5c6f67fdfe50a00ac572688e331adf6f2e62d707
Christian Bannes <cbannes@gmail.com>
2013-10-03 22:53:46 +0200

    cleaned up implementation of SimpleExpression

--- src/main/java/de/java/regexdsl/model/RegexExpression.java
+++ src/main/java/de/java/regexdsl/model/RegexExpression.java
@@ -2,9 +2,32 @@ package de.java.regexdsl.model;
 
 import java.util.List;
 
+/**
+ * Represents a regular expression. A regular expression can be seen as a tree where capturing groups 
+ * make up nodes and regexes without any capturing groups make up the leafs. This interface represents
+ * a node or a leaf. For leafs the method {{@link #getChildren()}} must return an empty list.
+ * 
+ * @author Christian Bannes
+ */
 public interface RegexExpression {
+	/** 
+	 * The regular expression as string 
+	 * @return the regex string, never null.
+	 **/
 	public String asRegex();
+
+	/** 
+	 * The children of this regex, must be empty for leafs. The children represent the capturing groups
+	 * of this regex.
+	 * 
+	 * @return the children, never null.  
+	 **/
 	public List<RegexExpression> getChildren();
+	
+	/** 
+	 * 
+	 * @return
+	 */
 	public int ignoreCapturingGroups();
 	public String getName();
 }
--- src/main/java/de/java/regexdsl/model/SimpleExpression.java
+++ src/main/java/de/java/regexdsl/model/SimpleExpression.java
@@ -2,34 +2,62 @@ package de.java.regexdsl.model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public abstract class SimpleExpression implements RegexExpression{
+/**
+ * A simple expression 
+ * @author Christian Bannes
+ */
+public abstract class SimpleExpression implements RegexExpression {
 
 
+	/**
+	 * @return always null
+	 */
+	@Override
 	public String getName() {
 		return null;
 	}
 	
+	/**
+	 * A simple expression does not have children per definition. For expressions 
+	 * with children use a {@link ComplexExpression}.
+	 * 
+	 * @return an empty list
+	 */
 	@Override
 	public List<RegexExpression> getChildren() {
 		return Collections.emptyList();
 	}
 
 
+	/**
+	 * Counts and returns the number of capturing groups of this regex expression.
+	 * @return the number of capturing groups
+	 */
 	public int ignoreCapturingGroups() {
-		final String s1 = this.asRegex().replaceAll("\\(", "");
-		final String s2 = this.asRegex();
-		final String s3 = this.asRegex().replaceAll("\\(\\?", ""); //embedded flag expression don't build up capturing groups!
-		final String s4 = this.asRegex().replaceAll("\\\\\\(", "");  //escaped braces
+		final int potentialGroups = countMatches(this.asRegex(), "\\("); //capturing groups start with braces
+		final int embeddedFlagExp = countMatches(this.asRegex(), "\\(\\?"); //embedded flag expressions don't build up capturing groups
+		final int escapedBraces = countMatches(this.asRegex(), "\\\\\\("); //dont count escaped braces as capturing groups
 		
-		
-		final int groups = s2.length() - s1.length();
-		final int embeddedFlagExp = (s2.length() - s3.length()) / 2;
-		final int escapedBraces = (s2.length() - s4.length()) / 2;
-		
-		return groups - embeddedFlagExp - escapedBraces;//s2.length() - s1.length(); //+ s3.length() / 2;
+		return potentialGroups - embeddedFlagExp - escapedBraces;
 	}
 	
+	/**
+	 * Counts the number of matches of the regex in the given string
+	 * @param s the string
+	 * @param regex the regex to match
+	 * @return the number of matches
+	 */
+	private int countMatches(final String s, final String regex) {
+		final Matcher m = Pattern.compile(regex).matcher(s);
+		int count = 0;
+		while(m.find())
+			count++;
+		
+		return count;
+	}
 	
 	
 }
